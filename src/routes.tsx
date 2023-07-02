af08b2ce888ecd4c8c05135f37c2d4283c67b586
Risyandi <risyandi.94@gmail.com>
2023-07-02 20:55:52 +0700

    fix: handle error page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,15 +2,18 @@ import { lazy } from "react";
 
 const HomePage = lazy(() => import("./pages/Home"));
 const CollectionPage = lazy(() => import("./pages/Collection"));
+const ErrorPage = lazy(() => import("./components/ErrorPage"));
 
 const routes = [
   {
     path: "/collections",
     element: <CollectionPage />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "*",
     element: <HomePage />,
+    errorElement: <ErrorPage />,
   },
 ];
 
