a5a628eb1932830c6b8a6f42f989a510a269f0cc
Risyandi <risyandi.94@gmail.com>
2023-07-03 10:40:29 +0700

    fix: integration with graphql
    work in progress because  need refactory code

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,14 @@
-import { Link } from 'react-router-dom';
+import {useEffect, useState} from 'react';
 import MainLayout from '../../common/MainLayout';
+import FooterLayout from '../../common/FooterLayout';
+import CardList from '../../components/CardList';
 import { GET_ANIME_LIST } from '../../utils/queryGraphApolloClient';
 import { useQuery } from '@apollo/client';
 
 const HomePage = () => {
+  const [dataCards, setDataCards] = useState<{[key: string]: any}>([]);
 
-  const { loading, error, data } = useQuery(GET_ANIME_LIST, {
+  const { data } = useQuery(GET_ANIME_LIST, {
     variables: {
       search: "detective conan",
       page: 2,
@@ -13,19 +16,17 @@ const HomePage = () => {
     },
   });
 
-  console.log(loading, 'risyandi: loading');
-  console.log(error, 'risyandi: error');
-  console.log(data, 'risyandi: data');
+  useEffect(() => {
+    if (data !== undefined) {
+      setDataCards(data.Page.media);
+    }
+  }, [data]);
 
   return (
   <div>
     <MainLayout/>
-    <p>Homepage</p>
-    <p>
-      <strong>
-        <Link to='/collections'>Collections</Link>
-      </strong>
-    </p>
+    <CardList data={dataCards}/>
+    <FooterLayout />
   </div>
   );
 };
