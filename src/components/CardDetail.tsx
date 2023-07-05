c4492743d2c86db5f26dd14f4abbb368a05c5f4b
Risyandi <risyandi.94@gmail.com>
2023-07-05 11:18:32 +0700

    fix: optimize fetch data and styles

--- src/components/CardDetail.tsx
+++ src/components/CardDetail.tsx
@@ -1,6 +1,6 @@
 /**
  * for handling error, using emotion in reactJS
- * error message : You have tried to stringify object returned from `css` function. It isn't supposed to be used directly (e.g. as value of the `className` prop), but rather handed to emotion so it can handle it (e.g. as value of `css` prop).
+ * error message : You have tried to stringify object returned from `css` function. It isn't supposed to be used directly (e.g. as value of the `cssName` prop), but rather handed to emotion so it can handle it (e.g. as value of `css` prop).
  */
 /** @jsxImportSource @emotion/react */
 
@@ -8,45 +8,94 @@ import { useEffect, useState } from "react";
 import { css } from "@emotion/react";
 import { useParams } from "react-router-dom";
 import { GET_ANIME_LIST_BY_ID } from "../utils/queryGraphApolloClient";
+import { convertHtmlToText } from "../utils/generalUtils";
 import { useQuery } from "@apollo/client";
 
-const cardStyles = css`
-  display: flex;
-  flex-direction: row;
-  border-radius: 8px;
-  margin: auto;
-  margin-top: 50px;
-  width: 70vw;
+const breakpoints = [768];
+const mq = breakpoints.map((bp) => `@media (max-width: ${bp}px)`);
+
+const articleCardStyles = css`
+  display: grid;
+  grid-template-columns: 285px minmax(300px, 445px);
+  max-width: 730px;
+  border-radius: 10px;
   box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
     rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
   @media (max-width: 600px) {
     flex-direction: column;
   }
+
+  ${mq[0]} {
+    grid-template-columns: 1fr;
+    max-width: 294px;
+    overflow: hidden;
+  }
 `;
 
-const leftColumnStyles = css`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+const imageBoxStyles = css`
+  border-radius: 10px 0 0 10px;
+  overflow: hidden;
+
+  ${mq[0]} {
+    border-radius: 0;
+    height: 180px;
+  }
 `;
 
-const rightColumnStyles = css`
-  flex: 4;
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-`;
-
-const imgStyles = css`
-  width: 400px;
-  height: 400px;
+const articleBannerStyles = css`
+  width: 100%;
+  height: 100%;
   object-fit: cover;
   object-position: left;
   transition: all 0.5s ease;
-  border-top-left-radius: 8px;
-  border-bottom-left-radius: 8px;
+
+  ${mq[0]} {
+  }
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+const articleContentStyles = css`
+  background: white;
+  padding: 32px 40px;
+  border-radius: 0 10px 10px 0;
+
+  ${mq[0]} {
+    border-radius: 0;
+    padding: 30px 28px 20px;
+  }
+`;
+
+const articleTitleStyles = css`
+  font-size: 20px;
+  color: black;
+  margin-bottom: 12px;
+  line-height: 1.4;
+
+  ${mq[0]} {
+    font-size: 15px;
+  }
+`;
+
+const articleTextStyles = css`
+  color: black;
+  font-size: 12px;
+  line-height: 1.5;
+  margin-bottom: 18px;
+
+  ${mq[0]} {
+    margin-bottom: 30px;
+  }
+`;
+
+const wrapperCardStyles = css`
+  margin: auto;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  display: grid;
+  place-items: center;
 `;
 
 type DetailHomePageParams = {
@@ -72,23 +121,23 @@ const CardDetail = () => {
   }, [data]);
 
   return (
-    <>
+    <div css={wrapperCardStyles}>
       {dataCardsDetail.map((card: any, index: any) => (
-        <div css={cardStyles} key={index}>
-          <div css={leftColumnStyles}>
+        <article key={index} css={articleCardStyles}>
+          <div css={imageBoxStyles}>
             <img
-              css={imgStyles}
               src={card.coverImage.large}
               alt={card.title.romaji}
+              css={articleBannerStyles}
             />
           </div>
-          <div css={rightColumnStyles}>
-            <h2>{card.title.romaji}</h2>
-            <p>{card.description}</p>
+          <div css={articleContentStyles}>
+            <h3 css={articleTitleStyles}>{card.title.romaji}</h3>
+            <p css={articleTextStyles}>{convertHtmlToText(card.description)}</p>
           </div>
-        </div>
+        </article>
       ))}
-    </>
+    </div>
   );
 };
 
