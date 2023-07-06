a76398223e6e7641645cf1e09907a20702898a34
Risyandi <risyandi.94@gmail.com>
2023-07-06 22:54:42 +0700

    Updates

--- src/react-app-env.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * what is the purpose of this file?
- * answer: https://stackoverflow.com/questions/67262914/what-is-the-react-app-env-d-ts-in-a-react-typescript-project-for
- */
-
-/// <reference types="react-scripts" />
